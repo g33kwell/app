17a1d5e98ca8f27f3e68d6e38a67486eb571c2ca
Youssef Boussetta <youssef.boussetta@outlook.com>
2018-03-10 00:21:14 +0000

    home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data = [
+    [{
+      'account' : '0-857210867-0 EUR',
+      'desc' : 'Description',
+      'balance' : '9276864387',
+      'hidden' : true
+    },{
+      'account' : '0-857210867-0 EUR',
+      'desc' : 'Description',
+      'balance' : '9276864387',
+      'hidden' : true
+    }],
+    [{
+      'account' : '0-857210867-0 EUR',
+      'desc' : 'Description',
+      'balance' : '9276864387',
+      'hidden' : true
+    },{
+      'account' : '0-857210867-0 EUR',
+      'desc' : 'Description',
+      'balance' : '9276864387',
+      'hidden' : true
+    }]
+  ];
 
+  selected = false;
+
+  constructor(public navCtrl: NavController) {
+    console.log(this.data)
   }
 
+  send(element){
+    if(element.hidden){
+      element.hidden = false;
+      this.selected = true;
+    }
+    else{
+      element.hidden = true;
+      this.selected = false;
+    }
+  }
 }
