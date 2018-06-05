7078762f08aaa35c258ad36e472c8e66f10ff817
Youssef Boussetta <youssef.boussetta@outlook.com>
2018-06-05 18:43:33 +0000

    Plugins working

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -15,6 +15,7 @@ import { FingerprintAIO } from "@ionic-native/fingerprint-aio";
 import { ScreenOrientation } from "@ionic-native/screen-orientation";
 import { IonSimpleWizard } from "../ion-simple-wizard/ion-simple-wizard.component";
 import { HomePage } from "../home/home";
+import { Keyboard } from "@ionic-native/keyboard";
 /**
  * Generated class for the AuthenticationPage page.
  *
@@ -28,11 +29,12 @@ import { HomePage } from "../home/home";
   templateUrl: "login.html"
 })
 export class LoginPage {
+  hideFinger: boolean = false;
   finger = false;
   isFirstStep = true;
   form: IArcotV6Form = <IArcotV6Form>{};
-  password ="azerty.123";
-  @ViewChild('wizard') wizard:IonSimpleWizard;
+  password = "azerty.123";
+  @ViewChild("wizard") wizard: IonSimpleWizard;
   @Output() loggedIn = new EventEmitter();
   //////////////////////////////////
   disabled: boolean = false;
@@ -43,8 +45,13 @@ export class LoginPage {
   stepsArray: Array<Object> = [];
   //////////////////////////////////
 
-  ionViewWillEnter() {
-    this.menuController.enable(false, 'myMenu');
+  ngOnInit() {
+    this.keyboard.onKeyboardShow().subscribe(() => {
+      this.hideFinger = true;
+    });
+    this.keyboard.onKeyboardHide().subscribe(() => {
+      this.hideFinger = false;
+    });
   }
 
   constructor(
@@ -57,74 +64,75 @@ export class LoginPage {
     public evts: Events,
     private screenOrientation: ScreenOrientation,
     private app: App,
-    private menuController: MenuController
+    private menuController: MenuController,
+    private keyboard: Keyboard
   ) {
-    
-    this.platform.ready().then( () => {
-        if(this.platform.is('cordova'))
-          this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT).then(() => console.log("locked"));
-        this.faio.isAvailable() ? (this.finger = true) : (this.finger = false);
+    this.menuController.enable(false, "myMenu");
+
+    this.platform.ready().then(() => {
+      if (this.platform.is("cordova"))
+        this.screenOrientation
+          .lock(this.screenOrientation.ORIENTATIONS.PORTRAIT)
+          .then(() => console.log("locked"));
+      this.faio.isAvailable() ? (this.finger = true) : (this.finger = false);
     });
     /**
-         * Step Wizard Settings
-         */
-    this.currentStep = 1;//The value of the first step, always 1
-    this.stepCondition = false;//For each step the condition is set to this value, Set to true if you don't need condition in every step
-    this.stepDefaultCondition = this.stepCondition;//Save the default condition for each step
+     * Step Wizard Settings
+     */
+    this.currentStep = 1; //The value of the first step, always 1
+    this.stepCondition = false; //For each step the condition is set to this value, Set to true if you don't need condition in every step
+    this.stepDefaultCondition = this.stepCondition; //Save the default condition for each step
     //Let's create some dummy data for this case
-    this.stepsArray =
-      [
-        {
-          title: 'Login Input',
-          name: 'login',
-          label: 'Subsriber ID',
-          icon : 'contact',
-          step: 1
-        },
-        {
-          title: 'Sms input',
-          name: 'smsOtp',
-          label: 'Sms Code',
-          icon : 'mail',
-          step: 2
-        },
-        {
-          title: 'Password input',
-          name: 'password',
-          label: 'Password',
-          icon : 'lock',
-          step: 3
-        }
-      ];
+    this.stepsArray = [
+      {
+        title: "Login Input",
+        name: "login",
+        label: "Subsriber ID",
+        icon: "contact",
+        step: 1
+      },
+      {
+        title: "Sms input",
+        name: "smsOtp",
+        label: "Sms Code",
+        icon: "mail",
+        step: 2
+      },
+      {
+        title: "Password input",
+        name: "password",
+        label: "Password",
+        icon: "lock",
+        step: 3
+      }
+    ];
     //You can subscribe to the Event 'step:changed' to handle the current step
-    this.evts.subscribe('step:changed', step => {
+    this.evts.subscribe("step:changed", step => {
       //Handle the current step if you need
       this.currentStep = step;
       //Set the step condition to the default value
       this.stepCondition = this.stepDefaultCondition;
-      this.toggleCondition()
+      this.toggleCondition();
     });
-    this.evts.subscribe('step:next', () => {
+    this.evts.subscribe("step:next", () => {
       //Do something if next
-     if (this.currentStep == 2) this.setSubscriberId();
-     else if (this.currentStep == 3) this.setSMS();
+      if (this.currentStep == 2) this.setSubscriberId();
+      else if (this.currentStep == 3) this.setSMS();
     });
-    this.evts.subscribe('step:back', () => {
+    this.evts.subscribe("step:back", () => {
       //Do something if back
-      console.log('Back pressed: ', this.currentStep);
+      console.log("Back pressed: ", this.currentStep);
     });
   }
 
   onKeyDown(e) {
     if (e.key === "Enter") {
-      if(this.currentStep == 3) this.setPassword()
-      else this.wizard.next()
+      if (this.currentStep == 3) this.setPassword();
+      else this.wizard.next();
     }
   }
 
-  setSubscriberId() {
-    
-  }
+  setSubscriberId() {}
 
   setSMS() {
     this.disabled = true;
@@ -140,13 +148,12 @@ export class LoginPage {
       setTimeout(() => {
         loading.dismiss().then(res => this.app.getRootNav().setRoot(HomePage));
       }, 1000);
-    }else {
+    } else {
       setTimeout(() => {
-        this.form.password = ""
+        this.form.password = "";
         loading.dismiss().then(res => this.presentAlert("Password inorrect!"));
       }, 1000);
     }
-    
   }
 
   checkFinger() {
@@ -166,29 +173,38 @@ export class LoginPage {
         loading.present();
 
         setTimeout(() => {
-          loading.dismiss().then(res => this.navCtrl.pop());
+          loading
+            .dismiss()
+            .then(res => this.app.getRootNav().setRoot(HomePage));
         }, 3000);
       })
       .catch((error: any) => console.log(error));
   }
 
   onFinish() {
-    this.setPassword()
+    this.setPassword();
   }
 
   toggleCondition() {
-    if(this.currentStep == 1) this.stepCondition = (this.form.login != "" && this.form.login != null)
-    else if(this.currentStep == 2) this.stepCondition = (this.form.smsOtp != "" && this.form.smsOtp != null && this.form.smsOtp.length == 6 && Number(this.form.smsOtp))
-    else if(this.currentStep == 3) this.stepCondition = (this.form.password != "" && this.form.password != null)
+    if (this.currentStep == 1)
+      this.stepCondition = this.form.login != "" && this.form.login != null;
+    else if (this.currentStep == 2)
+      this.stepCondition =
+        this.form.smsOtp != "" &&
+        this.form.smsOtp != null &&
+        this.form.smsOtp.length == 6 &&
+        Number(this.form.smsOtp);
+    else if (this.currentStep == 3)
+      this.stepCondition =
+        this.form.password != "" && this.form.password != null;
   }
 
   presentAlert(msg) {
     let alert = this.alertCtrl.create({
-      title: 'Error',
+      title: "Error",
       subTitle: msg,
-      buttons: ['Dismiss']
+      buttons: ["Dismiss"]
     });
     alert.present();
   }
-
 }
