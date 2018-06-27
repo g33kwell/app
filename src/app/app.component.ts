97f57058a423e57bab14e473b66812e9eb742550
BOUSSETTA Youssef <youssef.boussetta@soprabanking.com>
2018-06-27 12:19:28 +0100

    rectif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,7 +68,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       // let status bar overlay webview
-      statusBar.overlaysWebView(true);
+      statusBar.overlaysWebView(false);
 
       // set status bar to white
       statusBar.backgroundColorByHexString('#008983');
