a3eb89703c5a6d90847d64a754ff410a8dac668b
BOUSSETTA Youssef <youssef.boussetta@soprabanking.com>
2018-07-02 10:36:09 +0100

    Almost done :D

--- src/pages/order-cheque-book/order-cheque-book.ts
+++ src/pages/order-cheque-book/order-cheque-book.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App, AlertController, LoadingController } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the OrderChequeBookPage page.
@@ -15,11 +17,98 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OrderChequeBookPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  selectedAccount;
+  accounts;
+
+  leaf5 = true;
+  leaf10 = false;
+
+  quant1 = true;
+  quant2 = false;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dataProvider: DataProvider,
+    public app:App, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
+    this.accounts = this.dataProvider.getAccounts();
+    this.selectedAccount = this.accounts[0];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderChequeBookPage');
+  }
+
+  onChangeLeaves5() {
+    if(this.leaf5) this.leaf10 = false;
+  }
+
+  onChangeLeaves10() {
+    if(this.leaf10) this.leaf5 = false;
+  }
+
+  onChangeQuant1() {
+    if(this.quant1) this.quant2 = false;
+  }
+
+  onChangeQuant2() {
+    if(this.quant2) this.quant1 = false;
+  }
+
+  cancel() {
+    this.app.getRootNav().setRoot(HomePage);
+  }
+
+  confirmCheque() {
+    this.presentConfirm()
+  }
+
+  presentConfirm() {
+    let quant;
+    let leaf;
+
+    if(this.quant1) quant = 1;
+    else quant = 2;
+
+    if(this.leaf5) leaf = 5;
+    else leaf = 10;
+
+    let alert = this.alertCtrl.create({
+      title: 'Confirm order',
+      message: 'Do you want to order '+quant+' cheque book(s) of '+leaf+' leaves for the account '+this.selectedAccount.account+'?',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Order',
+          handler: () => {
+            let loading = this.loadingCtrl.create({
+              content: "Please wait..."
+            });
+    
+            loading.present();
+    
+            setTimeout(() => {
+              loading
+                .dismiss()
+                .then(val => this.presentAlert('Success','Your request is being processed'));
+            }, 1000);
+          }
+        }
+      ]
+    });
+
+    alert.present()
+
+  }
+
+  presentAlert(title, msg) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: msg,
+      buttons: ["Ok"]
+    });
+    alert.present();
   }
 
 }
