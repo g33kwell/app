2d3eec5e0faf1275317c54df2152cdad5d2052ad
BOUSSETTA Youssef <youssef.boussetta@soprabanking.com>
2018-07-03 17:28:31 +0100

    DONE

--- src/pages/cheque-inquiry/cheque-inquiry.ts
+++ src/pages/cheque-inquiry/cheque-inquiry.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
 import { LoginPage } from '../login/login';
+import { DataProvider } from '../../providers/data/data';
 
 /**
  * Generated class for the ChequeInquiryPage page.
@@ -16,7 +17,12 @@ import { LoginPage } from '../login/login';
 })
 export class ChequeInquiryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public app:App) {
+  selectedAccount;
+  accounts;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public app:App, public dataProvider: DataProvider) {
+    this.accounts = this.dataProvider.getAccounts();
+    this.selectedAccount = this.accounts[0];
   }
 
   ionViewDidLoad() {
